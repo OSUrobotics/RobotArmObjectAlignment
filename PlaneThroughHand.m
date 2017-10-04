6863785ca5d6e9a4a2f8eb27a0150f4f0bf98cc3
Cindy Grimm <cindy.grimm@oregonstate.edu>
2017-10-04 10:08:05 -0700

    Data for Xiaoli

---
+++ PlaneThroughHand.m
@@ -0,0 +1,31 @@
+function [idsInPlane, objPointsOriented ] = PlaneThroughHand( ptCenter, vecX, vecY, objPoints, height )
+%% PlaneThroughHand : Transform the object points into the given coordinate
+% system then return all of the object points that lie in the resulting x,z
+% plane
+% 
+% Input:
+%  ptCenter: R3 Pt that goes to the origin
+%  vecX: R3Vec that goes to the x direction
+%  vecY: R3Vec that goes to the y direction
+%  objPoints: R3 points + (optional) normals on the object
+%  height: How "thick" the slice should be - usually 1/8ish of the span of
+%  the hand, or 1/4 of the palm
+
+% Translate the points
+objPointsCenter = objPoints(:,1:3) - ptCenter;
+
+% Make the rotation matrix
+vecY = vecY ./ sqrt( vecY.*vecY );
+vecZ = cross( vecX, vecY );
+vecZ = vecZ ./ sqrt( vecZ .* vecZ );
+vecX = cross( vecY, vecZ );
+vecX = vecX ./ sqrt( vecX .* vecX );
+
+matRot = [ vecX; vecY; vecZ]';
+% Rotate
+objPointsOriented = matRot * objPointsCenter;
+
+% Clip
+idsInPlane = abs( objPointsOriented(:,2) ) < height;
+
+end
